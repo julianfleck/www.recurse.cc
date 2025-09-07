84b9c2ad548bde82803ace9aaa07d3812f932199
Julian Fleck <mail@julianfleck.net>
2025-09-08 00:56:14 +0200

    feat: use customized DocsLayout (DocsNavbar changes) and redirect /docs to Introduction

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,11 +1,11 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
-import { baseOptions } from '@/lib/layout.shared';
-import { source } from '@/lib/source';
+import { DocsLayout } from "@/components/layout/docs";
+import { baseOptions } from "@/lib/layout.shared";
+import { source } from "@/lib/source";
 
-export default function Layout({ children }: LayoutProps<'/docs'>) {
-  return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
-      {children}
-    </DocsLayout>
-  );
+export default function Layout({ children }: LayoutProps<"/docs">) {
+	return (
+		<DocsLayout {...baseOptions()} tree={source.pageTree} tabMode="navbar">
+			{children}
+		</DocsLayout>
+	);
 }
---
+++ app/docs/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from 'next/navigation';
+
+export default function Page() {
+  redirect('/docs/(0%20Guide)/introduction');
+}
+
+
