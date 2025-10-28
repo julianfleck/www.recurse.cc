21bba15e339eacb1e537e912f98d73cfcd0f3615
Julian Fleck <mail@julianfleck.net>
2025-10-27 23:10:39 -0400

    Phase 2: Create packages/fumadocs with shared config
    - Created icon resolver with Lucide and Tabler icons
    - Moved code highlighting themes
    - Created sidebar_label transformer
    - Set up proper package.json with dependencies

---
+++ packages/fumadocs/src/index.ts
@@ -0,0 +1,5 @@
+// Main exports for @recurse/fumadocs package
+export { resolveIcon } from './icons';
+export { themes, minimalAccentDark, minimalAccentLight } from './themes';
+export { sidebarLabelTransformer } from './transformers';
+
